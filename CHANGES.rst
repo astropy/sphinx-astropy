e414e6479c4322388e92c15844907e8e746a22d4
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-07 21:32:57 +0000

    Preparing release v1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes in sphinx-astropy
 =========================
 
-1.0 (unreleased)
+1.0 (2018-02-07)
 ----------------
 
 - Initial standalone version of this package (formerly packaged as part of astropy-helpers)
--- sphinx_astropy/__init__.py
+++ sphinx_astropy/__init__.py
@@ -1,10 +1,1 @@
-"""
-This package contains utilities and extensions for the Astropy sphinx
-documentation.  In particular, the `astropy.sphinx.conf` should be imported by
-the sphinx ``conf.py`` file for affiliated packages that wish to make use of
-the Astropy documentation format. Note that some sphinx extensions which are
-bundled as-is (numpydoc and sphinx-automodapi) are included in
-astropy_helpers.extern rather than astropy_helpers.sphinx.ext.
-"""
-
-__version__ = '1.0.dev0'
+__version__ = '1.0'
