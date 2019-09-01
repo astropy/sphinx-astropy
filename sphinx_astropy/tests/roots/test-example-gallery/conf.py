944551efaed017ab0d37d34434561097cad3e6e9
Jonathan Sick <jonathan@jsick.codes>
2019-08-31 21:12:57 -0400

    Enable intersphinx in example-gallery test case
    This will let us test intersphinx link resolution.

--- sphinx_astropy/tests/roots/test-example-gallery/conf.py
+++ sphinx_astropy/tests/roots/test-example-gallery/conf.py
@@ -1,8 +1,10 @@
 master_doc = 'index'  # for Sphinx <2.0
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
     'numpydoc',
     'sphinx_astropy.ext.example'
 ]
 exclude_patterns = ['_build']
 suppress_warnings = ['app.add_directive', 'app.add_node']
+default_role = 'obj'
